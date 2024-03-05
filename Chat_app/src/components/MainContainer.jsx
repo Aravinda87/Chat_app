b8bce2ccb91d3c4c833a2a53bae39345eba12fa5
Aravinda87 <aravinda.turuvekere@gmail.com>
2024-03-05 17:09:16 +0530

    first commit

---
+++ Chat_app/src/components/MainContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './myStyles.css';
+
+function MainContainer() {
+  return (
+    <div className='main-container'>MainContainer</div>
+  )
+}
+
+export default MainContainer 
