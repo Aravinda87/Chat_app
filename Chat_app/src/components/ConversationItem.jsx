53f36a6c6fc24ac19972aea741e00be7f0dfeb25
Aravinda87 <aravinda.turuvekere@gmail.com>
2024-03-06 13:15:54 +0530

    conversation section

---
+++ Chat_app/src/components/ConversationItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './myStyles.css';
+
+function ConversationItem({props}) {
+  return (
+    <div className='conversation-container'>
+        <p className='con-icon'>{props.name[0]}</p>
+        <p className='con-title'>{props.name}</p>
+        <p className='con-lastmessage'>{props.lastmessage}</p>
+        <p className='con-timestamp'>{props.timestamp}</p>
+    </div>
+  )
+}
+
+export default ConversationItem
--- Chat_app/src/components/Sidebar.jsx
+++ Chat_app/src/components/Sidebar.jsx
@@ -1,16 +1,37 @@
 import { IconButton } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import ConversationItem from './ConversationItem';
 
 
 function Sidebar() {
+
+    const [conversations,setconversation] = useState([
+        {
+            name : "Test1",
+            lastmessage : "lastmessage 1",
+            timestamp : "today"
+        },
+        {
+            name : "Test2",
+            lastmessage : "lastmessage 2",
+            timestamp : "today"
+        },
+        {
+            name : "Test3",
+            lastmessage : "lastmessage 3",
+            timestamp : "today"
+        },
+    ])
+
   return (
     <div className='sidebar-container'>
+        
         <div className='sb-header'>
             <div>
                 <IconButton>
@@ -33,14 +54,20 @@ function Sidebar() {
                 </IconButton>
             </div>
         </div>
+
         <div className='sb-search'>
             <IconButton>
             <SearchSharpIcon/>
             </IconButton>
             <input placeholder='search' className='search-box' />
         </div>
+
         <div className='sb-conversations'>
-            
+            {
+                conversations.map((conversation) => {
+                   return <ConversationItem props={conversation}/>
+                })
+            }
         </div>
     </div>
   )
