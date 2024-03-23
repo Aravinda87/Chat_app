035bb8bcaa7fc573877c49dd50869b18e78e2b3d
Aravinda87 <aravinda.turuvekere@gmail.com>
2024-03-24 03:35:52 +0530

    login

--- Chat_app/src/components/Login.jsx
+++ Chat_app/src/components/Login.jsx
@@ -93,9 +93,10 @@ function Login() {
     <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={loading}
-      ></Backdrop>
-
+      >
     <CircularProgress color="secondary" />
+      </Backdrop>
+
 
 
     <div className='login-container'>
